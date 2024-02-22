26171d8f8da314a1de6268af66b145a645383dd6
Svitlana <s.khlyntseva@gmail.com>
2024-02-22 16:11:57 +0200

    add multiplication

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ def add(x, y):
 def subtract(x,y):
     return x - y
 
+def multiplication(x, y):
+    return x*y
+
 if __name__ == "__main__":
     print("Simple Calculator App")
     
     try:
         num1 = int(input("Enter the first number: "))
-        operation = input("Enter operation (+ for addition, - for subtraction): ")
+        operation = input("Enter operation (+ for addition, - for subtraction, * for multiplication): ")
         num2 = int(input("Enter the second number: "))
     except ValueError:
         print("Invalid input. Please enter valid numbers.")
@@ -23,5 +26,8 @@ if __name__ == "__main__":
     elif operation == "-":
         result = subtract(num1, num2)
         print(f"{num1} - {num2} = {result}")
+    elif operation == "*":
+        result = multiplication(num1, num2)
+        print(f"{num1} * {num2} = {result}")    
     else:
         print("Invalid operation. Please enter + for addition or - for subtraction.")
