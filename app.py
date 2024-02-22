1e4d708186eedf2bfa23f9bfcb6c97507c177859
Svitlana <s.khlyntseva@gmail.com>
2024-02-22 15:56:28 +0200

    app done

---
+++ app.py
@@ -0,0 +1,27 @@
+def add(x, y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+if __name__ == "__main__":
+    print("Simple Calculator App")
+    
+    try:
+        num1 = int(input("Enter the first number: "))
+        operation = input("Enter operation (+ for addition, - for subtraction): ")
+        num2 = int(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+        exit(1)
+
+    
+
+    if operation == "+":
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+    elif operation == "-":
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+    else:
+        print("Invalid operation. Please enter + for addition or - for subtraction.")
